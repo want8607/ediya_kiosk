b0ac86ce900f3d59bc5c7ebd2ba9401ae960c9cd
want8607 <gotjd8607@gmail.com>
2022-03-06 22:23:37 +0900

    2022-03-06 22:23

--- app/src/main/java/com/example/stage/mainfragments/SettingFragment.kt
+++ app/src/main/java/com/example/stage/mainfragments/SettingFragment.kt
@@ -1,19 +1,22 @@
 package com.example.stage.mainfragments
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
+import androidx.preference.ListPreference
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.PreferenceManager
 import com.example.stage.MainActivity
 import com.example.stage.R
 
-class SettingFragment : PreferenceFragmentCompat() {
+class SettingFragment : PreferenceFragmentCompat(),SharedPreferences.OnSharedPreferenceChangeListener {
     lateinit var mainActivity : MainActivity
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-        addPreferencesFromResource(R.xml.setting)
+        setPreferencesFromResource(R.xml.setting,rootKey)
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
@@ -31,6 +34,17 @@ class SettingFragment : PreferenceFragmentCompat() {
             mainActivity.removeFragment(this,"setting")
         }
 
+        //모드 전환 설정
+        var appModePref = findPreference<ListPreference>("app_mode")
+        var prefs = PreferenceManager.getDefaultSharedPreferences(mainActivity)
+        fun setPreferenceSummary(preference: ListPreference,summary:String){
+            preference.summary = summary
+        }
+
+    }
+
+    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {
+        var p = findPreference<ListPreference>(key!!)
 
     }
 }
